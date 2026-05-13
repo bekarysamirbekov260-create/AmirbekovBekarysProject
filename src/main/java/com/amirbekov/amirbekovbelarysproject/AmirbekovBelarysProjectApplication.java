b78e2260ce1d66d7f0db49c9accbb6a8cdf7c9cc
bekar <bekarysamirbekov260@gmail.com>
2026-05-13 20:06:07 +0600

    Initial commit: Project structure and dependencies

---
+++ src/main/java/com/amirbekov/amirbekovbelarysproject/AmirbekovBelarysProjectApplication.java
@@ -0,0 +1,13 @@
+package com.amirbekov.amirbekovbelarysproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmirbekovBelarysProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmirbekovBelarysProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amirbekov/amirbekovbelarysproject/AmirbekovBelarysProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amirbekov.amirbekovbelarysproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmirbekovBelarysProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
